ad7da0fe6eb3c6a0ff0f1704d95c842c381301d2
grexn <nox-rox@web.de>
2021-11-28 21:54:47 +0100

    Add Shuffle page

--- .eleventy.js
+++ .eleventy.js
@@ -42,13 +42,13 @@ module.exports = function (eleventyConfig) {
         return content;
     });
 
-    eleventyConfig.addFilter("getFaceOfTheMonth", function(faces) {
-        const currentYearMonth = new Date().toISOString().slice(0,7);
+    eleventyConfig.addFilter('getFaceOfTheMonth', function (faces) {
+        const currentYearMonth = new Date().toISOString().slice(0, 7);
         let faceOfTheMonth = '';
 
         for (const face of faces) {
             if (!face.data.faceOfTheMonth) {
-                continue
+                continue;
             }
             for (const yearMonth of face.data.faceOfTheMonth) {
                 if (yearMonth === currentYearMonth) {
@@ -59,8 +59,8 @@ module.exports = function (eleventyConfig) {
         return faceOfTheMonth;
     });
 
-    eleventyConfig.addFilter("getFaceOfTheMonthCount", function(faceOfTheMonth) {
-        const currentYearMonth = new Date().toISOString().slice(0,7);
+    eleventyConfig.addFilter('getFaceOfTheMonthCount', function (faceOfTheMonth) {
+        const currentYearMonth = new Date().toISOString().slice(0, 7);
         let count = 0;
         try {
             for (const yearMonth of faceOfTheMonth) {
@@ -68,20 +68,20 @@ module.exports = function (eleventyConfig) {
                     count += 1;
                 }
             }
-        } catch (e){}
+        } catch (e) {}
         return count;
     });
 
-    eleventyConfig.addFilter("isNew", function(face) {
+    eleventyConfig.addFilter('isNew', function (face) {
         const faceBlockDate = new Date(face.data.date);
         let date6daysPast = new Date();
         date6daysPast.setDate(date6daysPast.getDate() - 6);
         return faceBlockDate.getTime() > date6daysPast.getTime();
     });
 
-    eleventyConfig.addFilter("isFaceOfTheMonth", function(face) {
+    eleventyConfig.addFilter('isFaceOfTheMonth', function (face) {
         if (!face.data.faceOfTheMonth) return false;
-        const currentYearMonth = new Date().toISOString().slice(0,7);
+        const currentYearMonth = new Date().toISOString().slice(0, 7);
         for (const yearMonth of face.data.faceOfTheMonth) {
             if (yearMonth === currentYearMonth) {
                 return true;
@@ -90,36 +90,49 @@ module.exports = function (eleventyConfig) {
         return false;
     });
 
-    eleventyConfig.addFilter("getLogoUrl", function(hodler) {
+    eleventyConfig.addFilter('getLogoUrl', function (hodler) {
         let logoUrl = '';
         if (hodler['logoIpfs']) {
             //logoUrl = `https://gateway.pinata.cloud/ipfs/${hodler.logoIpfs}`
-            logoUrl = `https://cloudflare-ipfs.com/ipfs/${hodler.logoIpfs}`
+            logoUrl = `https://cloudflare-ipfs.com/ipfs/${hodler.logoIpfs}`;
         } else if (hodler['logoLocal']) {
             logoUrl = hodler['logoLocal'];
         }
         return logoUrl;
     });
 
-    eleventyConfig.addFilter("getSortedHodlers", function(hodlers) {
-        let adjustedHodlers = hodlers.filter(hodler => parseInt(hodler.hodlCount) > 0)
+    eleventyConfig.addFilter('getSortedHodlers', function (hodlers) {
+        let adjustedHodlers = hodlers.filter((hodler) => parseInt(hodler.hodlCount) > 0);
 
         adjustedHodlers = adjustedHodlers.sort(function (hodlerA, hodlerB) {
             return parseInt(hodlerB.hodlCount) - parseInt(hodlerA.hodlCount);
-        })
+        });
 
         return adjustedHodlers;
     });
 
-    eleventyConfig.addFilter("dateToIso", function(date) {
-        return date.toISOString().slice(0,10);
+    eleventyConfig.addFilter('dateToIso', function (date) {
+        return date.toISOString().slice(0, 10);
+    });
+
+    eleventyConfig.addFilter('getFacesByPhysicalIds', function (faces, physicalFaceIds) {
+        let returnFaces = [];
+        const physicalFaceIdsArray = physicalFaceIds.split(',');
+
+        for (let face of faces) {
+            const facePhysicalNumber = face.data.title.split('#')[1];
+            if (physicalFaceIdsArray.includes(facePhysicalNumber)) {
+                returnFaces.push(face);
+            }
+        }
+        return returnFaces;
     });
 
     return {
         dir: {
             input: 'src',
             output: '_site',
-            data: '_data'
+            data: '_data',
         },
     };
 };
---
+++ src/shuffles/shuffle-1.md
@@ -0,0 +1,7 @@
+---
+title: "Stupid Face Shuffle #1"
+id: 1
+endDateTimestamp: 1638388800000
+shuffleFaces: 51,52,53,54,55
+mintPrice: 6
+---
