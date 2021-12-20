dcf34693cef4c0af1695052b4367c7f85dc2661b
grexn <nox-rox@web.de>
2021-12-20 15:53:00 +0100

    Add ARC69 attributes

--- build/shuffleBot/shuffleBot.js
+++ build/shuffleBot/shuffleBot.js
@@ -3,16 +3,16 @@ const glob = require('glob');
 const fs = require('fs');
 
 let filesToShuffle = [];
-const shuffleInputFiles = glob.sync(`${module.path}/../src/shuffles/stupidfaces-shuffle-*`);
-const shuffleResultIds = glob.sync(`${module.path}/../src/shuffles/shuffleResult-*`).map((shuffleResult) => shuffleResult.split('-')[1].split('.')[0]);
+const shuffleInputFiles = glob.sync(`${module.path}/../../src/shuffles/stupidfaces-shuffle-*`);
+const shuffleResultIds = glob
+    .sync(`${module.path}/../../src/shuffles/shuffleResult-*`)
+    .map((shuffleResult) => shuffleResult.split('-')[1].split('.')[0]);
 
 filesToShuffle = shuffleInputFiles.filter((inputFile) => !shuffleResultIds.includes(getShuffleIdByFilePath(inputFile)));
 
 for (const csvFilePath of filesToShuffle) {
     const shuffleId = getShuffleIdByFilePath(csvFilePath);
-    let fileLines = fs.readFileSync(csvFilePath)
-        .toString()
-        .split('\n');
+    let fileLines = fs.readFileSync(csvFilePath).toString().split('\n');
 
     fileLines = fileLines.slice(1);
 
@@ -20,20 +20,17 @@ for (const csvFilePath of filesToShuffle) {
 
     for (const line of fileLines) {
         const lineArray = line.split(',');
-        console.log('LINE_ARRAY:', lineArray)
         if (lineArray && lineArray.length > 1) {
             walletsInorder.push(lineArray[1].replace('"', '').slice(0, 15));
         }
-
     }
     console.log('walletsInorder: ', walletsInorder);
     const walletsShuffled = shuffle(walletsInorder);
 
     const json = JSON.stringify(walletsShuffled);
-    fs.writeFileSync(`${module.path}/../src/shuffles/shuffleResult-${shuffleId}.json`, json);
+    fs.writeFileSync(`${module.path}/../../src/shuffles/shuffleResult-${shuffleId}.json`, json);
 
     console.log('walletsShuffled: ', walletsShuffled);
-
 }
 
 function getShuffleIdByFilePath(fileName) {
